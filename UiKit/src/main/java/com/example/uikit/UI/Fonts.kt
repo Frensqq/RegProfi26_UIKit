b628101d9d7f426384860288df5d9bda530095b4
Frensqq <liadovn@yandex.ru>
2026-07-16 23:02:40 +0300

    create theme

--- UiKit/src/main/java/com/example/uikit/UI/Fonts.kt
+++ UiKit/src/main/java/com/example/uikit/UI/Fonts.kt
@@ -7,60 +7,56 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.uikit.R
 
-data class Type(
-    val Title1Semibold: TextStyle = TextStyle(),
-    val Title1Heavy: TextStyle = TextStyle(),
-    val Title2Regular: TextStyle = TextStyle(),
-    val Title2Semibold: TextStyle = TextStyle(),
-    val Title2Heavy: TextStyle = TextStyle(),
-    val Title3Regular: TextStyle = TextStyle(),
-    val Title3Medium: TextStyle = TextStyle(),
-    val Title3Semibold: TextStyle = TextStyle(),
-    val HeadlineRegular: TextStyle = TextStyle(),
-    val HeadlineMedium: TextStyle = TextStyle(),
-    val TextRegular: TextStyle = TextStyle(),
-    val TextMedium: TextStyle = TextStyle(),
-    val CaptionRegular: TextStyle = TextStyle(),
-    val CaptionSemibold:TextStyle = TextStyle(),
-    val Caption2Regular: TextStyle = TextStyle(),
-    val Caption2Bold:TextStyle = TextStyle()
-
+data class MatuleTypography(
+    val title1Semibold: TextStyle,
+    val title1Heavy: TextStyle,
+    val title2Regular: TextStyle,
+    val title2Semibold: TextStyle,
+    val title2Heavy: TextStyle,
+    val title3Regular: TextStyle,
+    val title3Medium: TextStyle,
+    val title3Semibold: TextStyle,
+    val headlineRegular: TextStyle,
+    val headlineMedium: TextStyle,
+    val textRegular: TextStyle,
+    val textMedium: TextStyle,
+    val captionRegular: TextStyle,
+    val captionSemibold: TextStyle,
+    val caption2Regular: TextStyle,
+    val caption2Bold: TextStyle
 )
 
-fun Typography():Type{
-
+fun createMatuleTypography(): MatuleTypography {
     fun textStyle(
-        fontFamily: Int,
-        fontSize:Int,
-        lineHeight:Int,
-        letterSpacing: Double = 0.00
-
+        fontRes: Int,
+        fontSize: Int,
+        lineHeight: Int,
+        letterSpacing: Float = 0f
     ) = TextStyle(
-        fontFamily = FontFamily(Font(fontFamily)),
+        fontFamily = FontFamily(Font(fontRes)),
         fontSize = fontSize.sp,
         lineHeight = lineHeight.sp,
         letterSpacing = letterSpacing.sp
     )
 
-    return Type(
-        Title1Semibold = textStyle(R.font.semibold, 24, 28 , 0.33),
-        Title1Heavy = textStyle(R.font.heavy, 24, 28 , 0.33),
-        Title2Regular = textStyle(R.font.regular, 20, 28 , 0.38),
-        Title2Semibold = textStyle(R.font.semibold, 20, 28 , 0.38),
-        Title2Heavy = textStyle(R.font.heavy, 20, 28 , 0.38),
-        Title3Regular = textStyle(R.font.regular, 17, 24 ),
-        Title3Medium = textStyle(R.font.medium, 17, 24 ),
-        Title3Semibold = textStyle(R.font.semibold, 17, 24 ),
-        HeadlineMedium = textStyle(R.font.medium,16,20,-0.32),
-        HeadlineRegular = textStyle(R.font.regular,16,20,-0.32),
-        TextRegular = textStyle(R.font.regular,15,20),
-        TextMedium = textStyle(R.font.medium,15,20),
-        CaptionRegular = textStyle(R.font.regular, 14, 20 ),
-        CaptionSemibold = textStyle(R.font.semibold, 14, 20 ),
-        Caption2Regular = textStyle(R.font.regular, 12, 20 ),
-        Caption2Bold = textStyle(R.font.bold, 12, 20 ),
-
+    return MatuleTypography(
+        title1Semibold = textStyle(R.font.semibold, 24, 28, 0.33f),
+        title1Heavy = textStyle(R.font.heavy, 24, 28, 0.33f),
+        title2Regular = textStyle(R.font.regular, 20, 28, 0.38f),
+        title2Semibold = textStyle(R.font.semibold, 20, 28, 0.38f),
+        title2Heavy = textStyle(R.font.heavy, 20, 28, 0.38f),
+        title3Regular = textStyle(R.font.regular, 17, 24),
+        title3Medium = textStyle(R.font.medium, 17, 24),
+        title3Semibold = textStyle(R.font.semibold, 17, 24),
+        headlineRegular = textStyle(R.font.regular, 16, 20, -0.32f),
+        headlineMedium = textStyle(R.font.medium, 16, 20, -0.32f),
+        textRegular = textStyle(R.font.regular, 15, 20),
+        textMedium = textStyle(R.font.medium, 15, 20),
+        captionRegular = textStyle(R.font.regular, 14, 20),
+        captionSemibold = textStyle(R.font.semibold, 14, 20),
+        caption2Regular = textStyle(R.font.regular, 12, 20),
+        caption2Bold = textStyle(R.font.bold, 12, 20)
     )
 }
 
-val MatuleType = Typography()
+val MatuleType = createMatuleTypography()
