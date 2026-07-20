899a94215f78495a435895d6a25c67594a9194f3
frens <liadovn@yandex.ru>
2026-07-21 01:45:29 +0300

    create toggle

---
+++ UiKit/src/main/java/com/example/uikit/Controls/Toggle.kt
@@ -0,0 +1,45 @@
+package com.example.uikit.Controls
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.DropShadowScope
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+import com.example.uikit.UI.MatuleTheme
+
+@Composable
+fun Toggle(state: Boolean, onClick: (Boolean) -> Unit){
+    val color = MatuleTheme.colors
+    Row(
+        modifier = Modifier
+            .width(48.dp)
+            .height(28.dp)
+            .clip(CircleShape)
+            .background(if (state) color.accent else color.inputStroke)
+            .padding(4.dp),
+        horizontalArrangement = if (!state) Arrangement.Start else Arrangement.End
+
+    ) {
+        Box(
+           modifier = Modifier
+               .size(24.dp)
+               .clip(CircleShape)
+               .background(color.white)
+               .clickable{onClick(!state)}
+               //.shadow(shape = CircleShape, elevation = 2.dp, ambientColor = color.black.copy(alpha = 0.15f))
+        )
+    }
+
+
+}
