9f83e4612879dc16f42d00ea23a7e0d3ccd21c30
frens <liadovn@yandex.ru>
2026-07-20 23:34:01 +0300

    create big button

---
+++ UiKit/src/main/java/com/example/uikit/Buttons/ButtonBig.kt
@@ -0,0 +1,70 @@
+package com.example.uikit.Buttons
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.uikit.UI.MatuleColor
+import com.example.uikit.UI.MatuleTheme
+import com.example.uikit.UI.MatuleTypography
+import com.example.uikit.UI.SpacerH
+import com.example.uikit.UI.createMatuleTypography
+
+@Composable
+fun ButtonBig(
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean
+){
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        modifier = Modifier.height(56.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonColors(
+            containerColor = MatuleColor.Accent,
+            contentColor = MatuleColor.White,
+            disabledContainerColor = MatuleColor.AccentInactive,
+            disabledContentColor = MatuleColor.White
+        ),
+    ) {
+        Text(
+            text,
+            style = createMatuleTypography().title3Semibold,
+            textAlign = TextAlign.Center,
+            color = MatuleColor.White
+        )
+    }
+
+}
+
+
+@Preview
+@Composable
+fun testButtons(){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        ButtonBig("test", {}, true)
+        SpacerH(5)
+        ButtonBig("tset2", {}, false)
+
+    }
+
+}
