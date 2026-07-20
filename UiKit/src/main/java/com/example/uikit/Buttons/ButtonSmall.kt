3933b7b4c751c20127bc7bfcbe20c18696b06513
frens <liadovn@yandex.ru>
2026-07-20 23:59:03 +0300

    create small button

--- UiKit/src/main/java/com/example/uikit/Buttons/ButtonSmall.kt
+++ UiKit/src/main/java/com/example/uikit/Buttons/ButtonSmall.kt
@@ -1,73 +1,58 @@
 package com.example.uikit.Buttons
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.uikit.UI.MatuleTheme
-import com.example.uikit.UI.MatuleTypography
-import com.example.uikit.UI.SpacerH
 import com.example.uikit.UI.createMatuleTypography
 
 @Composable
-fun ButtonBig(
-    text: String,
-    onClick: () -> Unit,
-    enabled: Boolean
+fun ButtonSmall(text: String,
+                onClick: () -> Unit,
+                state: Boolean = true,
+                enabled: Boolean = true
+
 ){
     val color = MatuleTheme.colors
     Button(
         onClick = onClick,
         enabled = enabled,
-        modifier = Modifier.height(56.dp)
-            .fillMaxWidth(),
+        modifier = Modifier.height(40.dp)
+            .width(96.dp),
         shape = RoundedCornerShape(10.dp),
-        colors = ButtonColors(
+        colors = if (state) {ButtonColors(
             containerColor = color.accent,
             contentColor = color.white,
             disabledContainerColor = color.accentInactive,
             disabledContentColor = color.white
-        ),
+        )}
+        else{
+            ButtonColors(
+                containerColor = color.white,
+                contentColor = color.accent,
+                disabledContainerColor = color.white,
+                disabledContentColor = color.accentInactive
+            )
+        },
+        border = BorderStroke(width = 1.dp, color =  color.accent),
+        contentPadding = PaddingValues(10.dp)
     ) {
         Text(
             text,
             style = createMatuleTypography().title3Semibold,
+            modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
         )
-    }
-
-}
-
-
-@Preview
-@Composable
-fun testButtons(){
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-
-        ButtonBig("test", {}, true)
-        SpacerH(5)
-        ButtonBig("tset2", {}, false)
-        SpacerH(5)
-        ButtonSmall("tset2", {}, true)
-        SpacerH(5)
-        ButtonSmall("tset2", {}, false)
-
     }
 
 }
