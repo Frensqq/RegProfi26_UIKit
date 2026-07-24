e12fd34c1bfca71b186cb3d9cf367a1e079b1772
frens <liadovn@yandex.ru>
2026-07-24 23:52:00 +0300

    create tabbar

---
+++ UiKit/src/main/java/com/example/uikit/Tabbar/Tabbar.kt
@@ -0,0 +1,80 @@
+package com.example.uikit.Tabbar
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.uikit.R
+import com.example.uikit.UI.MatuleTheme
+import com.example.uikit.UI.SpacerH
+
+@Composable
+fun TabBar(home: ()-> Unit, catalog: () -> Unit, orders: () -> Unit, profile: () -> Unit, current: String){
+
+    var currentState by remember { mutableStateOf(current) }
+    Column(
+        Modifier.fillMaxWidth().height(88.dp)
+    ) {
+        Box(Modifier.fillMaxWidth().height(1.dp).background(MatuleTheme.colors.description))
+
+        SpacerH(8)
+
+        Row(
+            Modifier.fillMaxWidth().padding(start = 7.dp, end = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            TabBarItem({
+                currentState = it
+            }, painterResource(R.drawable.home),
+                text = "Главная",
+                state = "Главная" == currentState
+                )
+            TabBarItem({
+                currentState = it
+            }, painterResource(R.drawable.catalog),
+                text = "Каталог",
+                state = "Каталог" == currentState
+                )
+            TabBarItem({
+                currentState = it
+            }, painterResource(R.drawable.orders),
+                text = "Заказы",
+                state = "Заказы" == currentState
+                )
+            TabBarItem({
+                currentState = it
+            }, painterResource(R.drawable.profile),
+                text = "Профиль",
+                state = "Профиль" == currentState
+                )
+
+
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TabBarPreview(){
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        TabBar({},{},{},{},"Заказы")
+    }
+}
---
+++ UiKit/src/main/java/com/example/uikit/Tabbar/TabbarItem.kt
@@ -0,0 +1,41 @@
+package com.example.uikit.Tabbar
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+import com.example.uikit.UI.MatuleTheme
+import com.example.uikit.UI.createMatuleTypography
+
+@Composable
+fun TabBarItem(OnClick: (String) -> Unit, icon: Painter, text: String, state: Boolean){
+
+    Column(
+        modifier = Modifier.height(49.dp).width(79.dp).padding(bottom = 1.dp)
+            .clickable{OnClick(text)},
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Icon(painter = icon,
+            contentDescription = null,
+            tint = if (state) MatuleTheme.colors.accent else MatuleTheme.colors.inputIcon)
+
+        Text(text,
+            style = createMatuleTypography().caption2Regular,
+            color = if (state) MatuleTheme.colors.accent else MatuleTheme.colors.inputIcon
+
+        )
+    }
+
+
+}
